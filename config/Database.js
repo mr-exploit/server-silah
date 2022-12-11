6bc7c787550288a7a6af77cb9ee2042f7d3f7da1
mr-exploit <raffiaj8@gmail.com>
2022-12-11 19:51:23 +0700

    fix bugs host value

--- config/Database.js
+++ config/Database.js
@@ -1,10 +1,11 @@
 import { Sequelize } from "sequelize";
 //utk koneksi mysql
-const db = new Sequelize('silah_db', 'root', '', {
+const db = new Sequelize('silah_db', '', {
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     host: "database-1.cwltokssd1g2.ap-southeast-1.rds.amazonaws.com",
     port: 3306,
     user: "admin",
+    password: "Kambing1011",
     dialect: "mysql"
 });
 
